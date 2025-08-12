d9bdf25f9fdd3e6455f7f5e4f5401b9a685009c8
Marco <marco.aurelio1451@gmail.com>
2025-08-12 14:47:20 -0300

    Modificação na documentação

--- js/launcher.js
+++ js/launcher.js
@@ -2,18 +2,14 @@ const chkCorreios = document.getElementById('chk-correios');
 const chkMoto     = document.getElementById('chk-moto');
 const chkSpx      = document.getElementById('chk-SPX');
 
-// Função para lidar com o clique nos checkboxes
 async function handleCheck(box, modulePath, fnName) {
   if (!box.checked) return;
 
-  // Salva o checkbox que foi selecionado localmente
   localStorage.setItem('selectedCheckbox', box.id);
 
-  // Atualiza a página
   location.reload();
 }
 
-// Ao carregar a página, verifica se havia um checkbox salvo e executa a função correspondente
 window.addEventListener('DOMContentLoaded', async () => {
   const savedCheckbox = localStorage.getItem('selectedCheckbox');
 
@@ -38,18 +34,15 @@ window.addEventListener('DOMContentLoaded', async () => {
         break;
     }
 
-    // Desmarca os outros checkboxes
     [chkCorreios, chkMoto, chkSpx]
       .filter(cb => cb.id !== savedCheckbox)
       .forEach(cb => cb.checked = false);
 
-    // Carrega o módulo e executa a função
     const mod = await import(modulePath);
     mod[fnName]();
   }
 });
 
-// Associa eventos aos checkboxes
 chkCorreios.addEventListener('change', () =>
   handleCheck(chkCorreios, './pages/correios/main_correios.js', 'main_correios')
 );
